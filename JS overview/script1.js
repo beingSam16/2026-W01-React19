1b2329312e27c28f34acfae347b8c9d60b07872b
beingSam16 <jamkarsameer75888@gmail.com>
2026-02-21 11:15:55 +0530

    added folder

---
+++ JS overview/script1.js
@@ -0,0 +1,13 @@
+/*variable declaration*/
+
+var x=10;
+var x=20;
+
+console.log("var x= "+x);
+console.log("re-declaration of x=" +x);
+
+var a=1;
+if(a!=0){
+    var z=200;
+}
+console.log("var dec inside loop still prints"+z);
